4b9bf738dfb22dfb97f2063e7d579b499d10b9a5
Td <860111114@qq.com>
2023-01-04 12:19:11 +0800

    ReggieTakeout v1.1

---
+++ ReggieTakeout/src/test/java/com/cwj/reggie/GitTest.java
@@ -0,0 +1,13 @@
+package com.cwj.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class GitTest {
+    @Test
+    public void main(){
+        log.info("Hello World");
+        log.info("Hello Git");
+    }
+}
